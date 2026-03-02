ed9edffed7e98601f0d6583e677dc2e6eac23d9d
mohammedafnas50-7 <mohammedafnas50@gmail.com>
2026-03-02 10:15:22 +0530

    UC2

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,7 +1,24 @@
-public class palindromecheckerapp {
+import java.util.Scanner;
+public class palindromecheckerapp{
     public static void main(String[] args){
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");;
         System.out.println("System initialized successfully");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the string");
+        String str = input.nextLine();
+        int r = str.length()-1;
+        int l =0;
+        while(l<r){
+            if (str.charAt(l)!=str.charAt(r)){
+                System.out.println("It is not a palindrome");
+                break;
+            }
+            l++;
+            r--;
+        }
+        if(l>=r){
+            System.out.println("Its a palindrome");
+        }
     }
 }
