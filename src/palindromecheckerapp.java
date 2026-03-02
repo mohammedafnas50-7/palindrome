cd3680a891448810537f37eec883ae94349a9bd1
mohammedafnas50-7 <mohammedafnas50@gmail.com>
2026-03-02 10:33:30 +0530

    UC6

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,26 +1,29 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
 public class palindromecheckerapp{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a string");
         Stack<Character> stack = new Stack<>();
+        Queue <Character> que = new LinkedList<>();
         String str = input.nextLine();
         int size = str.length();
         for ( char c :str.toCharArray()){
             stack.push(c);
+            que.add(c);
         }
         int i =0;
         while(!stack.isEmpty()){
-            int top = stack.pop();
-            if (top!=str.charAt(i)){
+            if (stack.pop()!=que.remove()){
                 System.out.println("It is not a palindrome");
                 break;         }
             i++;
         }
         if (i==size){
-            System.out.println("IT is a palindrome");
+            System.out.println("It is a palindrome");
         }
     }
 }
