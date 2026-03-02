4022662cdd9873ff0dafbb124d915fc508599ab2
mohammedafnas50-7 <mohammedafnas50@gmail.com>
2026-03-02 10:44:13 +0530

    UC9

--- src/palindromecheckerapp.java
+++ src/palindromecheckerapp.java
@@ -1,65 +1,33 @@
 import java.util.Scanner;
 
+public class palindromecheckerapp {
 
-public class palindromecheckerapp{
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter a string");
-        class Node {
-            char data;
-            Node next;
-            Node(char data){
-                this.data = data;
-                this.next=null;
-            }
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) {
+            return true;
         }
-        Node head=null;
-        Node prev=null;
-        String str = input.nextLine();
-        int size = str.length();
-        for ( char c :str.toCharArray()){
-            Node newNode = new Node(c);
-            if (head == null){
-                head = newNode;
-                prev = newNode;
-            }
-            else{
-                prev.next = newNode;
-                prev= prev.next;
-            }
+
+        if (str.charAt(start) != str.charAt(end)) {
+            return false;
         }
-        int i =0;
-        Node fast=head;
-        Node slow =head;
-        while(fast!=null && fast.next!=null){
-            fast=fast.next.next;
-            slow=slow.next;
+
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        input = input.replaceAll("\\s+", "").toLowerCase();
+
+        if (isPalindrome(input, 0, input.length() - 1)) {
+            System.out.println("The string is a Palindrome.");
+        } else {
+            System.out.println("The string is NOT a Palindrome.");
         }
-        Node head2=slow.next;
-        slow.next=null;
-        Node next ;
-        Node pr=null;
-        Node cu = head2;
-        while (cu!=null){
-            next=cu.next;
-            cu.next=pr;
-            pr=cu;
-            cu=next;
-        }
-        boolean ispalindrome = true;
-        while (head!=null && head2!=null){
-            if (head.data!=head2.data){
-                ispalindrome=false;
-                break;
-            }
-            head = head.next;
-            head2=head2.next;
-        }
-        if (ispalindrome){
-            System.out.println("It is a palindrome");
-        }
-        else{
-            System.out.println("It is not a palindrome");
-        }
+
+        scanner.close();
     }
 }
